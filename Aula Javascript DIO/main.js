cf1848d464c3a52736ba2f94569d211c6a9f2466
GabrielBragaa <gbragamoura@hotmail.com>
2022-03-22 23:11:24 -0300

    curso de js dio

---
+++ Aula Javascript DIO/main.js
@@ -0,0 +1,63 @@
+
+function trocar() {
+    document.getElementById("mousemove").innerHTML = "Mudou!";
+}
+
+function redirecionar() {
+    window.open("https://www.google.com.br");
+    window.location
+}
+
+function botao() {
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+    /*alert("Valeu!");*/
+}
+
+/*var idade = prompt("Qual a sua idade?");
+function validaIdade(idade) {
+    var validar;
+    if (idade >= 18) {
+        validar = true;
+    } else {
+        validar = false;
+    }
+    return validar;
+}
+ 
+ console.log(validaIdade(idade));*/
+
+
+
+/*var d = new Date();
+
+alert(d.getMinutes());*/
+
+/*var count;
+for (count=0; count <= 5; count++) {
+    alert(count);
+}*/
+
+/*var count = 0;
+while (count <= 5) {
+    console.log(count);
+    count++;
+}*/
+
+/*var idade = prompt("Qual a sua idade?");
+
+if (idade >= 18) {
+    alert("Maior de idade");
+} else {
+    alert("Menor de idade");
+}*/
+
+
+/*var frutas = [{nome: "maçã", cor: "vermelha"}, {nome: "uva", cor: "roxa"}];
+console.log("A cor da " + frutas[0].nome + " é " + frutas[0].cor);
+console.log("A cor da " + frutas[1].nome + " é " + frutas[1].cor);*/
+
+/* var lista = ["maçã", "pera", "laranja"];
+lista.pop("laranja");
+console.log(lista.toString());
+console.log(lista.join(" - "));
+alert(lista[0]); */
